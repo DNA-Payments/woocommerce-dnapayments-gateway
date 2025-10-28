c0cc0c7a9bd9e3e2ed5f281790bd5fef1ef1ca67
Nurbol Sarsenbayev <nurbol@dnapayments.com>
2025-10-28 18:29:20 +0500

    Update the update_order_status AJAX action hooks to include the gateway ID prefix, so they don't conflict with other AJAX actions using the same name.

--- assets/js/blocks/dnapayments_googlepay.asset.php
+++ assets/js/blocks/dnapayments_googlepay.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '5397676adbf6828dbc4e');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '52661a2d89555ab7f44b');

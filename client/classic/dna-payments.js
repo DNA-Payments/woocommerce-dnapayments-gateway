eef92667676d0258ba7378f517de7370c6c505f1
Nurbol Sarsenbayev <nurbol@dnapayments.com>
2025-11-13 08:09:07 +0500

    Fixed render error when payment component is not loaded

--- client/classic/dna-payments.js
+++ client/classic/dna-payments.js
@@ -15,7 +15,7 @@ import { getGlobalVariables, getRequiredFields } from './utils/data'
 import { validate } from './utils/validate'
 
 import { fetchPaymentAndAuthData } from '../common/api/fetch-payment-and-auth-data'
-import { getPaymentComponentErrorMessage } from '../common/payment-component-helper'
+import { getPaymentComponentErrorMessage, isInitFailed } from '../common/payment-component-helper'
 import { completePayment, getOrderIdFromPaymentData } from '../common/complete-payment'
 import { request } from '../common/api/request'
 import { debounce } from '../common/debounce'
@@ -275,16 +275,12 @@ jQuery(function ($) {
 
                 const message = getPaymentComponentErrorMessage(err, errorMessage)
 
-                if (paymentMethodObject.isLoaded && paymentMethodId === 'dnapayments_apple_pay') {
-                    return
-                }
-
-                if (message !== errorMessage) {
-                    showError(message)
-                } else {
+                if (!paymentMethodObject.isLoaded) {
                     paymentMethodObject.isLoading = false
                     $container.html(wrapMessage(errorMessage))
                     $container.css('height', 'auto')
+                } else if (paymentMethodId !== GATEWAY_ID_APPLE_PAY || !isInitFailed(err)) {
+                    showError(message)
                 }
             },
             onLoad: () => {

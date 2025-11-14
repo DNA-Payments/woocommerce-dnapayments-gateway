6ad47f081eae65d7ba6c69f99c33e9b98df2ea22
Nurbol Sarsenbayev <nurbol@dnapayments.com>
2025-11-14 19:28:05 +0500

    Add WooCommerce Subscriptions Support

--- includes/gateways/class-wc-gateway-dna-paypal.php
+++ includes/gateways/class-wc-gateway-dna-paypal.php
@@ -10,6 +10,10 @@ class WC_Gateway_DNA_PayPal extends WC_Gateway_DNA_Base_Payment_Component {
 		$this->icon          = WC_DNA_Payments::plugin_url() . '/assets/img/paypal.svg';
 		$this->method_title  = 'PayPal';
 		$this->method_description = 'PayPal provided by DNA Payments';
+		$this->supports     = array(
+            'products', 
+            'refunds',
+        );
 
 		parent::__construct();
 	}

b3dcb94ff15339b1d87792ae97784ab7303b0c1e
Nurbol Sarsenbayev <nurbol@dnapayments.com>
2025-11-17 13:06:22 +0500

    Show user-friendly error when Apple Pay / Google Pay fails to render

--- assets/js/blocks/dnapayments_googlepay.asset.php
+++ assets/js/blocks/dnapayments_googlepay.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'ca6666789fc275236574');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '8fa2f593155b4ecd1359');

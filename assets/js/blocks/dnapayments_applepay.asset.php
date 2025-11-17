87c5b75c7f1fb8a0a80c764701db8e49f2cec087
Nurbol Sarsenbayev <nurbol@dnapayments.com>
2025-11-17 16:07:30 +0500

    Suppress Apple Pay initialization/validation error when button already rendered (Block based Checkout)

--- assets/js/blocks/dnapayments_applepay.asset.php
+++ assets/js/blocks/dnapayments_applepay.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '46a0b49bcc6e2db09a7c');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '8211b3ae8cb2c47a13ae');
--- assets/js/blocks/dnapayments_googlepay.asset.php
+++ assets/js/blocks/dnapayments_googlepay.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '8fa2f593155b4ecd1359');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'f72d317505c82e98da4f');

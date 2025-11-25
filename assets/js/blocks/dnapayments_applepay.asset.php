f55bb0346e8476324280e79b1349f340f77b4cdf
Nurbol Sarsenbayev <nurbol@dnapayments.com>
2025-11-25 16:42:39 +0500

    Update Logic for Checking Apple Pay Availability

--- assets/js/blocks/dnapayments_applepay.asset.php
+++ assets/js/blocks/dnapayments_applepay.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '1db3870f07c71a66a83f');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '80911065b895b58a7097');

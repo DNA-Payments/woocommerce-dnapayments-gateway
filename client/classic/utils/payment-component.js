4c8e97c44d1d6b032c638f97bcbdf1e049a5aec5
Nurbol Sarsenbayev <nurbol@dnapayments.com>
2025-12-04 16:29:41 +0500

    Improving the rendering flow of the payment components for Apple Pay, Google Pay, and PayPal.

---
+++ client/classic/utils/payment-component.js
@@ -0,0 +1,56 @@
+import {
+    getPaymentComponentObject,
+    getPaymentComponentErrorMessages,
+    getPaymentComponentErrorMessage,
+} from '../../common/payment-component-helper'
+
+export function initPaymentComponent(paymentMethodId, options, ctx) {
+    const { paymentData } = ctx
+    const paymentMethodObject = getPaymentComponentObject(paymentMethodId)
+    const { initErrorMessage, validationErrorMessage } = getPaymentComponentErrorMessages(paymentMethodId)
+
+    return new Promise((resolve, reject) => {
+        paymentMethodObject.isLoaded = false
+
+        if (!paymentData) {
+            return reject(validationErrorMessage)
+        }
+
+        // Watch the WooCommerce payment-methods list so we can detect if the user switches
+        // to another gateway (which removes the current container from the DOM).
+        // When that happens we stop observing and resolve the promise so the caller
+        // can clean up or abort the component initialisation.
+        const parentElement = document.querySelector('#payment .payment_methods')
+        const observer = new MutationObserver((mutations) => {
+            if (!parentElement.contains(options.containerElement)) {
+                observer.disconnect()
+                resolve()
+            }
+        })
+        observer.observe(parentElement, { childList: true, subtree: true })
+
+        const events = {
+            ...options.events,
+            onError: (err) => {
+                observer.disconnect()
+                const message = getPaymentComponentErrorMessage(err, initErrorMessage)
+                if (!paymentMethodObject.isLoaded) {
+                    reject(initErrorMessage)
+                } else {
+                    options.events.onError(message)
+                }
+            },
+            onLoad: () => {
+                paymentMethodObject.isLoaded = true
+                observer.disconnect()
+                resolve()
+            },
+        }
+
+        paymentMethodObject.init({
+            ...options,
+            events,
+            paymentData,
+        })
+    })
+}

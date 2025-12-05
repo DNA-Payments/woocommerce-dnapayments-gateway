e49b76784c7cb303d4797f051229ec03216b4662
Nurbol Sarsenbayev <nurbol@dnapayments.com>
2025-12-05 08:15:35 +0500

    v4.1.0: Added PayPal button payment integration and improved payment component flows  - Added PayPal button payment integration via DNA Payments.  - Improved Apple Pay, Google Pay, and PayPal component rendering and availability checks.  - Enhanced clarity of user-facing error messages.  - Updated process_payment to refresh AJAX nonces and prevent 403 errors after guest-to-account session changes.

--- woocommerce-gateway-dnapayments.php
+++ woocommerce-gateway-dnapayments.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce DNA Payments Gateway
  * Plugin URI: https://www.dnapayments.com
  * Description: Take credit card payments on your store.
- * Version: 4.0.10
+ * Version: 4.1.0
  *
  * Author: DNA Payments Integration
  * Author URI: https://www.dnapayments.com
@@ -41,7 +41,7 @@ class WC_DNA_Payments {
 	public static $id = 'dnapayments';
 
 	// Plugin version
-	public static $version = '4.0.10';
+	public static $version = '4.1.0';
 
 	// Wordpress supported min version
 	public static $wp_min_version = '4.2';

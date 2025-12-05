969011fbff16408166938893ca2cbfe8eb584efa
Daulet-dna <Daulet-dna@users.noreply.github.com>
2025-12-05 09:58:39 +0500

    Send Verification support configuration during subscription “Change payment method” flow in WooCommerce plugin

--- client/classic/utils/place-order.js
+++ client/classic/utils/place-order.js
@@ -1,7 +1,7 @@
 import { debounce } from '../../common/debounce'
 import errors from '../../common/errors'
 import { payHostedFields } from '../../common/pay-hosted-fields'
-import { getSubscriptionPaymentMethods, hasSubscription } from '../../common/subscription'
+import { getSubscriptionPaymentMethods, hasSubscription, getSubscriptionVerificationConfig } from '../../common/subscription'
 import { shouldHideOrderLines } from '../../common/validater'
 import { getGlobalVariables } from './data'
 
@@ -56,6 +56,13 @@ export const createPlaceOrder = ({
         const _paymentMethods = hasSubscription(paymentData) ? getSubscriptionPaymentMethods() : paymentMethods
         if (_paymentMethods) {
             config.paymentMethods = _paymentMethods
+        }
+
+        const isChangePaymentMethod = page === 'change_payment_method';
+
+        if (isChangePaymentMethod) {
+            const verificationConfig = getSubscriptionVerificationConfig();
+            Object.assign(config, verificationConfig);
         }
 
         window.DNAPayments.configure(config)

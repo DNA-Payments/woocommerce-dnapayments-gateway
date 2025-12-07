e7487436ae6175fcf9ffa2aa7270eba34f2030f5
Nurbol Sarsenbayev <nurbol@dnapayments.com>
2025-12-07 18:15:17 +0500

    Calculate the cart totals before generating the payment data from the cart.

--- woocommerce-gateway-dnapayments.php
+++ woocommerce-gateway-dnapayments.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce DNA Payments Gateway
  * Plugin URI: https://www.dnapayments.com
  * Description: Take credit card payments on your store.
- * Version: 4.1.0
+ * Version: 4.1.1-dev
  *
  * Author: DNA Payments Integration
  * Author URI: https://www.dnapayments.com
@@ -41,7 +41,7 @@ class WC_DNA_Payments {
 	public static $id = 'dnapayments';
 
 	// Plugin version
-	public static $version = '4.1.0';
+	public static $version = '4.1.1-dev';
 
 	// Wordpress supported min version
 	public static $wp_min_version = '4.2';

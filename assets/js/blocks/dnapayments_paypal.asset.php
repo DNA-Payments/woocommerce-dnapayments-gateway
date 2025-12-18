867f471655ad6ef66d3d924674c99d0185e6b198
Daulet-dna <Daulet-dna@users.noreply.github.com>
2025-12-18 17:36:36 +0500

    (fix) Place Order button remains disabled after rapid switching between payment methods (block based checkout)

--- assets/js/blocks/dnapayments_paypal.asset.php
+++ assets/js/blocks/dnapayments_paypal.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '65db85a9360026729c2e');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '4c817d12b536d7a89d66');
--- client/blocks/utils/place-order-button.js
+++ client/blocks/utils/place-order-button.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from '@wordpress/element'
+import { useEffect, useRef, useCallback } from '@wordpress/element'
 import { GATEWAY_ID_APPLE_PAY, GATEWAY_ID_GOOGLE_PAY, GATEWAY_ID_PAYPAL } from '../../common/constants'
 import { useCheckoutUpdate } from '../hooks/use-checkout-update'
 
@@ -6,46 +6,72 @@ export function isPlaceOrderButtonDisabled(activePaymentMethod) {
     return [GATEWAY_ID_GOOGLE_PAY, GATEWAY_ID_APPLE_PAY, GATEWAY_ID_PAYPAL].includes(activePaymentMethod)
 }
 
-export function setPlaceOrderButtonDisabled(isDisabled, button) {
-    const placeOrderButton = button || getPlaceOrderButton()
-
-    if (!placeOrderButton) {
-        return
-    }
-
-    if (isDisabled) {
-        placeOrderButton.setAttribute('disabled', 'disabled')
-    } else {
-        placeOrderButton.removeAttribute('disabled')
-    }
-}
-
-export function triggerPlaceOrderButtonClick() {
-    const placeOrderButton = getPlaceOrderButton()
-
-    if (placeOrderButton) {
-        placeOrderButton.removeAttribute('disabled')
-        placeOrderButton.click()
-    }
-}
+const DNA_PLACE_ORDER_DISABLED_ATTR = 'data-dnapayments-place-order-disabled'
 
 export function getPlaceOrderButton() {
     return document.querySelector('button.wc-block-components-checkout-place-order-button')
 }
 
+export function setPlaceOrderButtonDisabled(isDisabled, button) {
+    const placeOrderButton = button || getPlaceOrderButton()
+    if (!placeOrderButton) return
+
+    if (isDisabled) {
+        placeOrderButton.setAttribute('disabled', 'disabled')
+        placeOrderButton.setAttribute(DNA_PLACE_ORDER_DISABLED_ATTR, '1')
+        return
+    }
+
+    // Включаем обратно только если мы сами выключали
+    if (placeOrderButton.hasAttribute(DNA_PLACE_ORDER_DISABLED_ATTR)) {
+        placeOrderButton.removeAttribute('disabled')
+        placeOrderButton.removeAttribute(DNA_PLACE_ORDER_DISABLED_ATTR)
+    }
+}
+
+export function triggerPlaceOrderButtonClick() {
+    const placeOrderButton = getPlaceOrderButton()
+    if (!placeOrderButton) return
+
+    placeOrderButton.removeAttribute('disabled')
+    placeOrderButton.click()
+}
+
 export function useTogglePlaceOrderButtonDisabled(activePaymentMethod) {
     const refActivePaymentMethod = useRef(activePaymentMethod)
-    const update = () => {
-        const isDisabled = isPlaceOrderButtonDisabled(refActivePaymentMethod.current)
-        const button = getPlaceOrderButton()
-        setTimeout(() => setPlaceOrderButtonDisabled(isDisabled, button), 100)
-    }
+    const timeoutRef = useRef(null)
+
+    const clearPendingTimeout = useCallback(() => {
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current)
+            timeoutRef.current = null
+        }
+    }, [])
+
+    const scheduleUpdate = useCallback(() => {
+        clearPendingTimeout()
+
+        timeoutRef.current = setTimeout(() => {
+            const button = getPlaceOrderButton()
+            const isDisabled = isPlaceOrderButtonDisabled(refActivePaymentMethod.current)
+            setPlaceOrderButtonDisabled(isDisabled, button)
+
+            timeoutRef.current = null
+        }, 100)
+    }, [clearPendingTimeout])
 
     useEffect(() => {
         refActivePaymentMethod.current = activePaymentMethod
-    }, [activePaymentMethod])
+        scheduleUpdate()
+    }, [activePaymentMethod, scheduleUpdate])
 
-    useCheckoutUpdate(update)
+    useCheckoutUpdate(scheduleUpdate)
 
-    update()
+    useEffect(() => {
+        return () => {
+            clearPendingTimeout()
+            // On unmount, re-enable only if it was disabled by our code.
+            setPlaceOrderButtonDisabled(false)
+        }
+    }, [clearPendingTimeout])
 }

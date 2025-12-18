92a3c797b4e072e499ba71ed22bbbad58336beaa
Daulet-dna <Daulet-dna@users.noreply.github.com>
2025-12-18 17:36:36 +0500

    (fix) Place Order button remains disabled after rapid switching between payment methods (block based checkout)

--- assets/js/blocks/dnapayments_applepay.asset.php
+++ assets/js/blocks/dnapayments_applepay.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '87818decaf42f5e01358');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '7477ba43eb0e7c45d3e6');
--- assets/js/blocks/dnapayments_googlepay.asset.php
+++ assets/js/blocks/dnapayments_googlepay.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '4b420606f88e7e1883ce');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'd0cb3c7e2dbd0f07bb85');

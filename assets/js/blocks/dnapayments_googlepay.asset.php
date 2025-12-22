7006cd4497978c6cfda34787cd434e47590354e9
Nurbol Sarsenbayev <nurbol@dnapayments.com>
2025-12-22 18:47:20 +0500

    Show loader during redirect delay after Google Pay / Apple Pay payment on block-based checkout

--- assets/js/blocks/dnapayments_googlepay.asset.php
+++ assets/js/blocks/dnapayments_googlepay.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'd0cb3c7e2dbd0f07bb85');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '6e61449a2dfa84e731b1');
--- assets/js/blocks/dnapayments_paypal.asset.php
+++ assets/js/blocks/dnapayments_paypal.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '4c817d12b536d7a89d66');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'fbbaa6e2ab4adfbad3b7');

0e7563623c0b5d4c998577f89b887c8236c2eb60
nurzhas <nurzhas.nurbayev@gmail.com>
2026-09-10 11:55:22 +0500

    Hide cc error banner after debit entered

--- client/classic/dna-payments-add-card.js
+++ client/classic/dna-payments-add-card.js
@@ -45,7 +45,7 @@ jQuery(function ($) {
                     hostedFieldsInstance = instance
                     cardError.hide()
                 },
-                onError: (errMsg) => cardError.show(errMsg),
+                onError: (errMsg) => (errMsg ? cardError.show(errMsg) : cardError.hide()),
             })
         }
     }
